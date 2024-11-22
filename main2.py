76f4571f745a75eb1d42baffab633774237f3c30
frbzk <fireblack20030501@gmail.com>
2024-11-22 19:56:07 +0800

    11/22_ou

--- main2.py
+++ main2.py
@@ -388,8 +388,14 @@ def main():
             else:
                 print(f"unvalid code!")
 
+def test():
+    
+    return
+
+
 
 if __name__ == "__main__":
     tagset=TagSet()
     taskset=TaskSet()
-    main()
+    test()
+    
